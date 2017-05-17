ec3679b585c588541b4c39044062c22171d7ce64
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-17 21:42:36 +0200

    edit view
    edit inscription/connexion mot de passe as password

--- app/view/connexion.php
+++ app/view/connexion.php
@@ -9,7 +9,7 @@
 			Email : <br>
 			<input type="text" name="email"><br>
 			Mot de Passe : <br>
-			<input type="text" name="mdp"><br>
+			<input type="password" name="mdp"><br>
 			<input type="Submit" name="inscrit">
 		</form>
 	</body>
--- app/view/inscription.php
+++ app/view/inscription.php
@@ -9,9 +9,9 @@
 			Email : <br>
 			<input type="text" name="email"><br>
 			Mot de Passe : <br>
-			<input type="text" name="mdp"><br>
+			<input type="password" name="mdp"><br>
 			Confirmation mot de passe : <br>
-			<input type="text" name="Cmdp"><br>
+			<input type="password" name="Cmdp"><br>
 			<input type="Submit" name="inscrit">
 		</form>
 	</body>
