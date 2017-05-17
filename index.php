d5c75c95d93f984afe55a465f2ddc26a2aaa6278
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-17 18:33:11 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Adresse du root du server
+ */
+define("ROOT", realpath(__dir__));
+
+/**
+ * Adresse du dossier view sur le server
+ */
+define('VIEW_PATH', ROOT .'/app/view/');
+
+/**
+ * Adresse du dossier model sur le server
+ */
+define('MODEL_PATH', ROOT.'/app/model/');
+
+/**
+ * Adresse du dossier controller sur le server
+ */
+define('CONTROLLER_PATH', ROOT.'/app/controller/');
+
