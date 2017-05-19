58dbff23be10dec6b65d4dcfa6583dba4014e571
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-19 20:51:53 +0200

    edit model

--- app/model/Model.php
+++ app/model/Model.php
@@ -22,14 +22,13 @@ abstract class Model{
 			$dbname='d3ii5fodbsca3';
 			$host='ec2-54-228-255-234.eu-west-1.compute.amazonaws.com';
 			$dbuser='nyjmohehyilnvw';
-			$dbpass='963ae619d9edfe5580cf7278f372d5204dc59edd4c0e6209d59e91650be73495
-';
+			$dbpass='963ae619d9edfe5580cf7278f372d5204dc59edd4c0e6209d59e91650be73495';
 
 			try
 			{
 				$bd = new PDO("pgsql:host=$host;dbname=$dbname;charset=utf", $dbuser, $dbpass);
 				$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				return$bd
+				return $bd;
 			}
 			catch (PDOException $e)
 			{
