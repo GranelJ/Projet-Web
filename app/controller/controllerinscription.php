377ae3685e968fc03f52bfc39aa670a579f236bd
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-19 20:14:33 +0200

    edit controller
    edit controllerinscription

--- app/controller/controllerinscription.php
+++ app/controller/controllerinscription.php
@@ -2,9 +2,9 @@
 	require_once('../model/ModelUtilisateur.php');
 
 	if(isset($_POST['email']) AND isset($_POST['mdp']) AND isset($_POST['Cmdp'])){
-		$email = htmlentities($_POST['email'], ENT_QUOTES);
-		$mdp = htmlentities($_POST['mdp'], ENT_QUOTES);
-		$Cmdp = htmlentities($_POST['Cmdp'], ENT_QUOTES);
+		$email = htmlspecialchars($_POST['email'], ENT_QUOTES);
+		$mdp = htmlspecialchars($_POST['mdp'], ENT_QUOTES);
+		$Cmdp = htmlspecialchars($_POST['Cmdp'], ENT_QUOTES);
 		if ($mdp == $Cmdp){
 			$mdp = sha1($mdp);
 			$result = ModelUtilisateur::createUtilisateur($email, $mdp);
