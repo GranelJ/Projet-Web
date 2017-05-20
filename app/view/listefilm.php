6355395e397ea04f94fc040f75687e44f22d15ac
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-20 12:00:46 +0200

    edit view
    add button dahsboard for admin

--- app/view/listefilm.php
+++ app/view/listefilm.php
@@ -5,7 +5,11 @@
 	</head>
 
 	<body>
-		<?php include("");?>
+		<?php $droit = $_COOKIE("droit");
+		if($droit=sha1("admin")){
+			print('<a href="dashboard_admin.php">Dashboard</a>');
+		}
+		?>
 		<a href="ajoutfilm.php">Ajouter un film</a>
 	</body>
 </html>
