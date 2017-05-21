a7223242bfb27c1853920b665ff3b7cd2260f66e
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-21 11:48:54 +0200

    add view
    add view to add categorie as admin

---
+++ app/view/addcategorie.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>"Ajout catégorie"</title>
+    </head>
+
+    <body>
+        <form method="POST" action="/app/controller/controllerCategorieFilm.php">
+			Catégorie : <br>
+			<input type="text" name="namecat"><br>
+            <input type="Submit" name="Addcat">
+        </form>
+    </body>
+</html>
