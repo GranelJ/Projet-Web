73825c1bb4c192fd84e41ba9b4d7b973f5d69486
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-22 17:13:39 +0200

    edit testco

--- app/controller/controllertestconnexion.php
+++ app/controller/controllertestconnexion.php
@@ -1,9 +1,9 @@
 <?php
-    if((null !==$_COOKIE(['droit'])) AND (null !==$_COOKIE(['info']))){
+    if((null!==$_COOKIE(['droit'])) AND (null!==$_COOKIE(['info']))){
         $droit = $_COOKIE(["droit"]);
-        if ($droit===sha1("admin")){
+        if ($droit==sha1("admin")){
             header("Location: /app/view/dashboard.php");
-        }elseif ($droit===sha1("util")){
+        }elseif ($droit==sha1("util")){
             header("Location: /app/view/listefilmutil.php");
         }
     }else{
