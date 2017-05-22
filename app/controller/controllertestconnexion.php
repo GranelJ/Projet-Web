11c49df5c2ea3a30ad2568ab40d58ddccc8abff8
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-22 14:10:44 +0200

    edit controller

--- app/controller/controllertestconnexion.php
+++ app/controller/controllertestconnexion.php
@@ -1,11 +1,14 @@
 <?php
     $info = $_COOKIE(["info"]);
     $droit = $_COOKIE(["droit"]);
-    if(($droit == null) AND ($info == null)){
+    if(isset($_COOKIE(["droit"])) AND isset($_COOKIE(["info"]))){
+        $droit = $_COOKIE(["droit"]);
+        if ($droit==sha1("admin")){
+            header("Location: /app/view/dashboard.php");
+        }elseif ($droit=sha1("util")){
+            header("Location: /app/view/listefilmutil.php");
+        }
+    }else{
         header("Location: /app/view/accueil.php");
-    }elseif($droit==sha1("admin")){
-        header("Location: /app/view/dashboardadmin.php");
-    }elseif($droit==sha1("util")){
-        header("Location: /app/view/listefilmutil.php");
     }
 ?>
