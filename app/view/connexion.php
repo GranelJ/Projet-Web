db817aea70d62e757b0da8b7713ac9c143b88e52
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-22 19:24:23 +0200

    edit connexion

--- app/view/connexion.php
+++ app/view/connexion.php
@@ -6,15 +6,24 @@
 	</head>
 
 	<body>
-		<form method="POST" action="/app/controller/controllerconnexion.php">
-			
-			<div class="input-group">
-  			<span class="input-group-addon" id="basic-addon1"></span>
-  			<input type="email" name="email" class="form-control" placeholder="Email" aria-describedby="basic-addon1">
-			<br>
-			Mot de Passe :
-			<input type="password" name="mdp"><br>
-			<input type="Submit" name="inscrit">
-		</form>
+		<section id="portfolio">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<h2>Connexion</h2>
+							<hr class="star-primary">
+								<p>
+								<form method="POST" action="/app/controller/controllerconnexion.php">
+									Email : <br>
+									<input type="text" name="email"><br>
+									Mot de Passe : <br>
+									<input type="password" name="mdp"><br>
+									<input type="Submit" name="inscrit">
+								</form>
+								</p>
+					</div>
+				</div>
+			</div>
+		</section>
 	</body>
 </html>
