bad7b015a5661e11fd57481fc7e5e90786cb020b
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-22 19:25:53 +0200

    reverse edit

--- app/view/connexion.php
+++ app/view/connexion.php
@@ -6,24 +6,12 @@
 	</head>
 
 	<body>
-		<section id="portfolio">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 text-center">
-						<h2>Connexion</h2>
-							<hr class="star-primary">
-								<p>
-								<form method="POST" action="/app/controller/controllerconnexion.php">
-									Email : <br>
-									<input type="text" name="email"><br>
-									Mot de Passe : <br>
-									<input type="password" name="mdp"><br>
-									<input type="Submit" name="inscrit">
-								</form>
-								</p>
-					</div>
-				</div>
-			</div>
-		</section>
+		<form method="POST" action="/app/controller/controllerconnexion.php">
+			Email : <br>
+			<input type="text" name="email"><br>
+			Mot de Passe : <br>
+			<input type="password" name="mdp"><br>
+			<input type="Submit" name="inscrit">
+		</form>
 	</body>
 </html>
