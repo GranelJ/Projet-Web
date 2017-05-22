3a79ec7294468cd9434716760a549597077b94fb
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-22 17:55:52 +0200

    edit add admin

--- app/controller/controlleraddadmin.php
+++ app/controller/controlleraddadmin.php
@@ -4,12 +4,17 @@
 	if(!empty($_POST['email']) AND !empty($_POST['mdp'])){
 		$email = htmlspecialchars($_POST['email']);
 		$mdp = htmlspecialchars($_POST['mdp']);
-		$mdp = sha1($mdp);
-		createAdministrateur($email, $mdp);
-		echo "Inscription validée";
+		if(!getIdAdmin($email)>0){
+			$mdp = sha1($mdp);
+			createAdministrateur($email, $mdp);
+			echo "Administrateur ajouté";
 		    header('refresh:3;url=/app/view/listefilm.php');
+			}else{
+				echo"Cet email est déjà utilisé";
+				header("refresh:2;url=/app/view/addadmin.php");
+			}
 		}else{
 			echo "Tous les champs ne sont pas remplis";
-			header("Location: /app/view/addadmin.php");
+			header("refresh:2;url=/app/view/addadmin.php");
 		}
 ?>
