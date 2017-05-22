764c5129540400a6521ae6c548982ded66d58e48
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-22 14:03:43 +0200

    edit controller

--- app/controller/controllertestconnexion.php
+++ app/controller/controllertestconnexion.php
@@ -1,6 +1,6 @@
 <?php
-    $info = $_COOKIE("info");
-    $droit = $_COOKIE("droit");
+    $info = $_COOKIE(["info"]);
+    $droit = $_COOKIE(["droit"]);
     if(($droit !== null) AND ($info !== null)){
         header("Location: /app/view/accueil.php");
     }elseif($droit==sha1("admin")){
