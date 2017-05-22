844592668e5ae6a5b952e2c9b012868cc766036c
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-22 18:56:52 +0200

    edit controller
    create film

--- app/controller/controllercreatefilm.php
+++ app/controller/controllercreatefilm.php
@@ -4,9 +4,9 @@
 	require_once('../model/ModelActeur.php');
 	require_once('../model/ModelRealisateur.php');
 
-	if(isset($_POST['NomFilm']) AND isset($_POST['Categorie']) AND isset($_POST['Annee']) AND isset($_POST['Vu']) AND 
-	isset($_POST['Note']) AND isset($_POST['NomRealisateur']) AND isset($_POST['PrenomRealisateur']) AND 
-	isset($_POST['NomPActeur']) AND isset($_POST['PrenomPActeur'])){
+	if(!empty($_POST['NomFilm']) AND !empty($_POST['Categorie']) AND !empty($_POST['Annee']) AND !empty($_POST['Vu']) AND 
+	!empty($_POST['Note']) AND !empty($_POST['NomRealisateur']) AND !empty($_POST['PrenomRealisateur']) AND 
+	!empty($_POST['NomPActeur']) AND !empty($_POST['PrenomPActeur'])){
 		$nomfilm = htmlspecialchars($_POST['NomFilm']);
 		$cat = htmlspecialchars($_POST['Categorie']);
 		$annee = htmlspecialchars($_POST['Annee']);
@@ -17,7 +17,7 @@
 		$nompacteur = htmlspecialchars($_POST['NomPActeur']);
 		$prenompacteur = htmlspecialchars($_POST['PrenomPActeur']);
 
-		/*$existact = getIdAct($nompacteur,$prenompacteur);
+		$existact = getIdAct($nompacteur,$prenompacteur);
 		if (!$existact>0){//si acteur existe pas 
 			createActeur($nompacteur,$prenompacteur);
 		}
@@ -25,12 +25,12 @@
 		$existreal = getIdReal($nomrealisateur, $prenomrealisateur);
 		if(!$existreal>0){//si realisateur existe pas
 			createRealisateur($nomrealisateur,$prenomrealisateur);
-		}*/
+		}
 
-		//$id_real = getIdReal($nomrealisateur, $prenomrealisateur);
-		//$id_act = getIdAct($nompacteur, $prenompacteur);
-		//$id_cat = getIdCat($cat);
-		//createFilm($nomfilm, $annee, $id_cat, $id_real, $id_act);
+		$id_real = getIdReal($nomrealisateur, $prenomrealisateur);
+		$id_act = getIdAct($nompacteur, $prenompacteur);
+		$id_cat = getIdCat($cat);
+		createFilm($nomfilm, $annee, $id_cat, $id_real, $id_act);
 		echo "Film ajouté, vous allez être redirigé vers le formulaire d'ajout de film.";
 		header('refresh:5;url=/app/view/ajoutfilm.php');
 		}
