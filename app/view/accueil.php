045c1f1f05b1e126441cdc608165a4cbef36276f
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-22 12:27:11 +0200

    edit accueil

--- app/view/accueil.php
+++ app/view/accueil.php
@@ -1,24 +1,13 @@
 <!DOCTYPE html>
-<?php
-	require_once("/app/controller/controllertestconnexion.php");
-	if (isconnected()){
-		$droit = $_COOKIE("droit");
-		if($droit==sha1("admin")){
-			header("Location: /app/view/dashboardadmin.php");
-		}elseif($droit==sha1("util")){
-			header("Location: /app/view/listefilmutil.php");
-		}else{ ?>
-			<html>
-				<head>
-					<title>Accueil</title>
-				</head>
 
-			<body>
-				<a href="connexion.php">Se connecter</a>
-				<a href="inscription.php">S'inscire</a>
-			</body>
-		</html>
-		<?php
-		}
-	}
-?>
+
+<html>
+	<head>
+		<title>Accueil</title>
+	</head>
+
+	<body>
+		<a href="connexion.php">Se connecter</a>
+		<a href="inscription.php">S'inscire</a>
+	</body>
+</html>
