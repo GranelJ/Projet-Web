9d3b514d83f83f6dc701e18a02b684337dc5b086
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-22 14:13:01 +0200

    edit dashboardadmin

--- app/view/dashboardadmin.php
+++ app/view/dashboardadmin.php
@@ -5,7 +5,7 @@
 	</head>
 
 	<body>
-		<a href="/app/view/ajoutcategorie.php">Ajouter une categorie</a>
+		<a href="/app/view/addcategorie.php">Ajouter une categorie</a>
 		<a href="/app/view/addadmin.php">Ajouter un admin</a>
 		<a href="/app/controller/controllerdeconnexion.php">Déconnexion</a>
 	</body>
