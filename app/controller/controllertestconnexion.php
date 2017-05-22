eab0586fb65594bdf4eb371efc830d41d9295bbc
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-22 17:05:35 +0200

    edit controller
    discard change createfilm, edit testco

--- app/controller/controllertestconnexion.php
+++ app/controller/controllertestconnexion.php
@@ -1,11 +1,9 @@
 <?php
-    $info = $_COOKIE(['info']);
-    $droit = $_COOKIE(['droit']);
     if(isset($_COOKIE(['droit'])) AND isset($_COOKIE(['info']))){
         $droit = $_COOKIE(["droit"]);
-        if ($droit==sha1("admin")){
+        if ($droit===sha1("admin")){
             header("Location: /app/view/dashboard.php");
-        }elseif ($droit=sha1("util")){
+        }elseif ($droit===sha1("util")){
             header("Location: /app/view/listefilmutil.php");
         }
     }else{
