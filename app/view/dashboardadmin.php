c2562f9981f8e8c943243aa470fb0dc711a209d7
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-24 15:11:09 +0200

    edit button

--- app/view/dashboardadmin.php
+++ app/view/dashboardadmin.php
@@ -6,11 +6,9 @@
 	</head>
 
 	<body>
-		<a class="btn btn-default" href="#" role="button">Lien</a>
-			<button type="button" class="btn btn-success">Ajouter une categorie</button>
-			<button type="button" class="btn btn-default">Ajouter un administrateur</button>
-			<button type="button" class="btn btn-default">Déconnexion</button>
-		</div>
+		<button type="button" class="btn btn-success">Ajouter une categorie</button>
+		<a href="/app/view/addcategorie.php" class="btn btn-primary btn-lg active" role="button" title="Lien 1">Lien 1</a>
+		<button type="button" class="btn btn-default">Déconnexion</button>
 		<a href="/app/view/addcategorie.php">Ajouter une categorie</a>
 		<a href="/app/view/addadmin.php">Ajouter un admin</a>
 		<a href="/app/controller/controllerdeconnexion.php">Déconnexion</a>
