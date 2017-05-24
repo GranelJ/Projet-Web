4cd9751b6e27e1737a8e6461796e7836ae7354bc
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-24 11:24:12 +0200

    test

--- app/controller/controllercreatefilm.php
+++ app/controller/controllercreatefilm.php
@@ -1,8 +1,8 @@
 <?php
-	require_once('../model/ModelFilm.php');
-	require_once('../model/ModelCategorieFilm.php');
-	require_once('../model/ModelActeur.php');
-	require_once('../model/ModelRealisateur.php');
+	require_once("../model/ModelFilm.php");
+	require_once("../model/ModelCategorieFilm.php");
+	require_once("../model/ModelActeur.php");
+	require_once("../model/ModelRealisateur.php");
 
 	
 ?>
