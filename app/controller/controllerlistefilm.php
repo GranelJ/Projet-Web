b767fd866b80c8080f78b525b4a181bd1c8366b6
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-24 10:25:26 +0200

    edit controllerlistefilm

--- app/controller/controllerlistefilm.php
+++ app/controller/controllerlistefilm.php
@@ -1,10 +1,9 @@
 <?php require_once("../model/ModelFilm.php");
-      require_once("../model/ModelAjouter.php");?>
+      require_once("../model/ModelAjouter.php");
 
-<?php
     $idutil = $_COOKIE("info");
     $film = selectbyutil($idutil);
     $idfilm=$film[id_film];
     $nomfilm=getNameFilmById($idfilm);
-    header("Location: /app/view/listefilmutil.php")
+    header("Location: /app/view/listefilmutil.php");
 ?>
