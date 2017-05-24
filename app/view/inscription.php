4a503208e446fdec28b8faf786b317c44595a692
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-24 16:07:39 +0200

    try fix css inscription

--- app/view/inscription.php
+++ app/view/inscription.php
@@ -9,11 +9,17 @@
 		<form method="POST" action="/app/controller/controllerinscription.php">
 			<div class="input-group">
 				<span class="input-group-addon" id="basic-addon1">Email</span>
-				<input type="email" name="email" class="form-control" aria-describedby="basic-addon1"><br>
+				<input type="email" name="email" class="form-control" aria-describedby="basic-addon1">
+			</div>
+			<br>
+			<div class="input-group">	
 				<span class="input-group-addon" id="basic-addon1">Mot de passe</span>
-				<input type="password" name="mdp" class="form-control" aria-describedby="basic-addon1"><br>
+				<input type="password" name="mdp" class="form-control" aria-describedby="basic-addon1">
+			</div>
+			<br>
+			<div class="input-group">	
 				<span class="input-group-addon" id="basic-addon1">Confirmation mot de passe</span>
-				<input type="password" name="Cmdp" class="form-control" aria-describedby="basic-addon1"><br>				
+				<input type="password" name="Cmdp" class="form-control" aria-describedby="basic-addon1">				
 			</div>
 			<input type="Submit" name="inscrit">
 			<a href="/app/view/accueil.php"><button type="button" class="btn btn-default">Retour</button></a>
