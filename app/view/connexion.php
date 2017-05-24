e76a06570b057ee4f5a97948a68dc10593370558
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-24 15:58:39 +0200

    fix connexion css

--- app/view/connexion.php
+++ app/view/connexion.php
@@ -9,9 +9,8 @@
 		<form method="POST" action="/app/controller/controllerconnexion.php">
 			<div class="input-group">
   				<input type="text" name="email" class="form-control" placeholder="Email" aria-describedby="basic-addon1">
+				<input type="password" name="mdp" class="form-control" placeholder="Mot de passe" aria-describedby="basic-addon1">
 			</div>
-			Mot de Passe : <br>
-			<input type="password" name="mdp"><br>
 			<input type="Submit" name="inscrit">
 		</form>
 	</body>
