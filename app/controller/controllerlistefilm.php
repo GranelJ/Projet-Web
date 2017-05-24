90bfb5e38a6282cfc99155e2b8a64584f3730971
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-24 10:40:17 +0200

    edit cookie's data recover

--- app/controller/controllerlistefilm.php
+++ app/controller/controllerlistefilm.php
@@ -2,7 +2,7 @@
     require_once("../model/ModelFilm.php");
     require_once("../model/ModelAjouter.php");
 
-    $idutil = $_COOKIE("info");
+    $idutil = $_COOKIE["info"];
     //$film = selectbyutil($idutil);
     //$idfilm=$film[id_film];
     //$nomfilm=getNameFilmById($idfilm);
