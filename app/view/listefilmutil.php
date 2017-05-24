f75e27bad889ec809c05517716ae0ff805e604c1
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-24 10:16:42 +0200

    try

--- app/view/listefilmutil.php
+++ app/view/listefilmutil.php
@@ -1,4 +1,4 @@
-
+<?php require_once("../controller/controllerlistefilm.php");?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,6 +9,24 @@
 	<body>
 		<a href="/app/view/ajoutfilm.php">Ajouter un film</a>
 		<a href="/app/controller/controllerdeconnexion.php">Déconnexion</a>
+		<table>
+			<tr>
+				<th>Nom</th>
+				<th>Annee</th>
+				<th>Note</th>
+				<th>Vu</th>
+			</tr>
+			<?php
+				while($ligne1=$nomfilm->fetch() && $ligne2=$film->fetch()){
+					echo"<tr>";
+					echo"<td>$ligne1[nom_film]</td>";
+					echo"<td>$ligne1[annee_film]</td>";
+					echo"<td>$ligne2[note_film]</td>";
+					echo"<td>$ligne2[vu]</td>";
+					echo"</tr>";
+				}
+			?>
+		</table>
 		<!--tous les films de l'utilisateur-->
 	</body>
 </html>
