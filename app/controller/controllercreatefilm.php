a5dba999e5ffc141c519c45cefbf26545a95899a
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-24 14:44:11 +0200

    try
    require insteed of require_once

--- app/controller/controllercreatefilm.php
+++ app/controller/controllercreatefilm.php
@@ -1,8 +1,8 @@
 <?php 
-	require_once("../model/ModelActeur.php");
-	require_once("../model/ModelRealisateur.php");
-	require_once("../model/ModelCategorieFilm.php");
-	require_once("../model/ModelFilm.php");
+	require("../model/ModelActeur.php");
+	require("../model/ModelRealisateur.php");
+	require("../model/ModelCategorieFilm.php");
+	require("../model/ModelFilm.php");
 
 	if(!empty($_POST['NomFilm']) AND !empty($_POST['Categorie']) AND !empty($_POST['Annee']) AND !empty($_POST['Vu']) AND 
 	!empty($_POST['Note']) AND !empty($_POST['NomRealisateur']) AND !empty($_POST['PrenomRealisateur']) AND 
