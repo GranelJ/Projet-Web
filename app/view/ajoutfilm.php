cafecdc957c6dc04504e6594eb01705f359f272d
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-24 21:09:31 +0200

    try debug

--- app/view/ajoutfilm.php
+++ app/view/ajoutfilm.php
@@ -18,7 +18,7 @@
 			Nom du film : <br>
 			<input type="text" name="NomFilm"><br>
 			Categorie : <br>
-			<select name="Categorie_Film">
+			<select name="Categorie_Film" size ="1">
 				<?php
 					while ($choix=$cat->fetch()){
 						echo "<option value=".$choix[0].">".$choix[0]."</option>";
