80c2694484b07ef5dd2dcd2c1bbf29a1dacbf08d
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-24 18:18:35 +0200

    try fix URI

--- app/controller/controllertestconnexion.php
+++ app/controller/controllertestconnexion.php
@@ -3,10 +3,10 @@
         $droit = $_COOKIE["droit"];
         if ($droit==sha1("admin")){
             //si admin
-            header("Location: /app/view/dashboardadmin.php");
+            header("Location: /admin/dashboard");
         }elseif ($droit==sha1("util")){
             //si utilisateur
-            header("Location: /app/view/listefilmutil.php");
+            header("Location: /user/listefilm");
         }
     }else{
         header("Location: /accueil");
