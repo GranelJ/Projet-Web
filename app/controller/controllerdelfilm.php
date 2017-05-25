84f0b692499989849288d72f42102f7b35f7318e
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-25 10:59:34 +0200

    debugging

--- app/controller/controllerdelfilm.php
+++ app/controller/controllerdelfilm.php
@@ -4,7 +4,7 @@
     if(!empty($_POST['namefilm']) AND !empty($_POST['annee'])){
         $name=htmlspecialchars($_POST['namefilm']);
         $annee=htmlspecialchars($_POST['annee']);
-       // $id=getIdFilm($name,$annee);
+        $id=getIdFilm($name,$annee);
        // deleteByIdFilm($id);
         echo "Le film a bien été supprimer";
         header("refresh:2;url=/user/listefilm");
