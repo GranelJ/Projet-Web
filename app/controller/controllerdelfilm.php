f009d5ec0973de92f67e0120074aee5effddc589
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-25 10:56:56 +0200

    test debug

--- app/controller/controllerdelfilm.php
+++ app/controller/controllerdelfilm.php
@@ -2,7 +2,7 @@
 	require_once("../model/ModelFilm.php");
     
     if(!empty($_POST['namefilm']) AND !empty($_POST['annee'])){
-       // $name=htmlspecialchars($_POST['namefilm']);
+        $name=htmlspecialchars($_POST['namefilm']);
        // $annee=htmlspecialchars($_POST['annee']);
        // $id=getIdFilm($name,$annee);
        // deleteByIdFilm($id);
