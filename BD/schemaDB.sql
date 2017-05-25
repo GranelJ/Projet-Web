6945201c641d2f210da185d0d6e5d74339d16f79
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-25 11:46:34 +0200

    fix script db

--- BD/schemaDB.sql
+++ BD/schemaDB.sql
@@ -40,17 +40,17 @@ CREATE TABLE film(
 	realisateur_film INTEGER NOT NULL,
 	acteur_film INTEGER NOT NULL,
 	CONSTRAINT PK_film PRIMARY KEY (id_film),
-	CONSTRAINT "film_fk1" FOREIGN KEY ("cat_film") REFERENCES categorie_film("id_catfilm"),
-	CONSTRAINT "film_fk2" FOREIGN KEY ("realisateur_film") REFERENCES realisateur("id_realisateur"),
-	CONSTRAINT "film_fk3" FOREIGN KEY ("acteur_film") REFERENCES acteur("id_acteur")
+	CONSTRAINT "film_fk1" FOREIGN KEY ("cat_film") REFERENCES categorie_film("id_catfilm") ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT "film_fk2" FOREIGN KEY ("realisateur_film") REFERENCES realisateur("id_realisateur") ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT "film_fk3" FOREIGN KEY ("acteur_film") REFERENCES acteur("id_acteur") ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE realiser(
 	id_film INTEGER,
 	id_realisateur INTEGER,
 	CONSTRAINT PK_realiser PRIMARY KEY (id_film, id_realisateur),
-	CONSTRAINT "realiser_fk1" FOREIGN KEY ("id_film") REFERENCES film("id_film"),
-	CONSTRAINT "realiser_fk2" FOREIGN KEY ("id_realisateur") REFERENCES realisateur("id_realisateur")
+	CONSTRAINT "realiser_fk1" FOREIGN KEY ("id_film") REFERENCES film("id_film") ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT "realiser_fk2" FOREIGN KEY ("id_realisateur") REFERENCES realisateur("id_realisateur") ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE ajouter(
@@ -59,30 +59,30 @@ CREATE TABLE ajouter(
 	id_film INTEGER,
 	id_utilisateur INTEGER,
 	CONSTRAINT PK_ajouter PRIMARY KEY (id_film, id_utilisateur),
-	CONSTRAINT "ajouter_fk1" FOREIGN KEY ("id_film") REFERENCES film("id_film"),
-	CONSTRAINT "ajouter_fk2" FOREIGN KEY ("id_utilisateur") REFERENCES utilisateur("id_utilisateur")
+	CONSTRAINT "ajouter_fk1" FOREIGN KEY ("id_film") REFERENCES film("id_film") ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT "ajouter_fk2" FOREIGN KEY ("id_utilisateur") REFERENCES utilisateur("id_utilisateur") ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE avoir(
 	id_cat INTEGER,
 	id_film INTEGER,
 	CONSTRAINT PK_avoir PRIMARY KEY (id_cat, id_film),
-	CONSTRAINT "avoir_fk1" FOREIGN KEY ("id_cat") REFERENCES categorie_film("id_catfilm"),
-	CONSTRAINT "avoir_fk2" FOREIGN KEY ("id_film") REFERENCES film("id_film")
+	CONSTRAINT "avoir_fk1" FOREIGN KEY ("id_cat") REFERENCES categorie_film("id_catfilm") ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT "avoir_fk2" FOREIGN KEY ("id_film") REFERENCES film("id_film") ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE jouer(
 	id_film INTEGER,
 	id_acteur INTEGER,
 	CONSTRAINT PK_jouer PRIMARY KEY (id_film, id_acteur),
-	CONSTRAINT "jouer_fk1" FOREIGN KEY ("id_film") REFERENCES film("id_film"),
-	CONSTRAINT "jouer_fk2" FOREIGN KEY ("id_acteur") REFERENCES acteur("id_acteur")
+	CONSTRAINT "jouer_fk1" FOREIGN KEY ("id_film") REFERENCES film("id_film") ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT "jouer_fk2" FOREIGN KEY ("id_acteur") REFERENCES acteur("id_acteur") ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE modifier(
 	id_administrateur INTEGER,
 	id_cat INTEGER,
-	CONSTRAINT PK_modifier PRIMARY KEY (id_administrateur, id_cat),
-	CONSTRAINT "modifier_fk1" FOREIGN KEY ("id_administrateur") REFERENCES administrateur("id_administrateur"),
-	CONSTRAINT "modifier_fk2" FOREIGN KEY ("id_cat") REFERENCES categorie_film("id_catfilm")
+	CONSTRAINT PK_modifier PRIMARY KEY (id_administrateur, id_cat) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT "modifier_fk1" FOREIGN KEY ("id_administrateur") REFERENCES administrateur("id_administrateur") ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT "modifier_fk2" FOREIGN KEY ("id_cat") REFERENCES categorie_film("id_catfilm") ON UPDATE CASCADE ON DELETE CASCADE
 );
