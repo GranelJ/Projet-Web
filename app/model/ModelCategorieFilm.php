ac7a6290a30b727a3dc2493d0f159b1ace0c8c62
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-25 11:17:52 +0200

    fix model categorie and delfilm(maybe)

--- app/model/ModelCategorieFilm.php
+++ app/model/ModelCategorieFilm.php
@@ -59,7 +59,7 @@ require_once("Model.php");
 	function getIdCat($lib){
 		global $bd;
 		try{
-			$req=$bd->prepare('SELECT id_catfilm WHERE lib_cat = ?');
+			$req=$bd->prepare('SELECT id_catfilm FROM categorie_film WHERE lib_cat = ?');
 			$req->execute(array($lib));
 			$res=$req->fetch();
 			return($res[0]);
