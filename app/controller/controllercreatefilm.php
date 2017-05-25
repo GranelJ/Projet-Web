1d583ccbf01521a50c524891925b956a84f33a42
jorisgranel <joris.granel@etu.umontpellier.fr>
2017-05-25 13:53:30 +0200

    try fix issue when film non vu

--- app/controller/controllercreatefilm.php
+++ app/controller/controllercreatefilm.php
@@ -8,10 +8,14 @@
 	if(!empty($_POST['nomFilm']) AND !empty($_POST['catFilm']) AND !empty($_POST['annee']) AND !empty($_POST['nomRealisateur']) 
 	AND !empty($_POST['prenomRealisateur']) AND !empty($_POST['nomPActeur']) AND !empty($_POST['prenomPActeur'])){
 		//si tous les champs remplis
+		if(empty($_POST['Vu'])){
+			$vu=false;
+		}else{
+			$vu=$_POST['Vu'];
+		}
 		$nomfilm = htmlspecialchars($_POST['nomFilm']);
 		$cat = htmlspecialchars($_POST['catFilm']);
 		$annee = htmlspecialchars($_POST['annee']);
-		$vu = htmlspecialchars($_POST['Vu']);
 		$note = htmlspecialchars($_POST['note']);
 		$nomrealisateur = htmlspecialchars($_POST['nomRealisateur']);
 		$prenomrealisateur = htmlspecialchars($_POST['prenomRealisateur']);
